2039205333becbf0ad8141ea0bc487e8464762f7
apple <taagoomacmini@apple.local>
2014-03-26 11:40:08 +0800

    Initial Commit

---
+++ ASIHTTPRequest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ASIHTTPRequest
+//
+//  Created by apple on 14-3-26.
+//  Copyright (c) 2014年 Taagoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
